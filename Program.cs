667796bba820c4be89958bc1a4c727abc66c45a6
Лев <jerrrardcornelius@gmail.com>
2022-09-06 15:17:01 +0300

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа принимает два числа и выдает какое число большее.
+string inputLineA = Console.ReadLine()??"";
+string inputLineB = Console.ReadLine()??"";
+
+    int inputNumberA = int.Parse(inputLineA);
+    int inputNumberB = int.Parse(inputLineB);
+
+    if (inputNumberA > inputNumberB)
+    {
+        Console.Write("max = ");
+        Console.Write(inputLineA);
+    }
+    else
+    {
+        Console.Write("max = ");
+        Console.Write(inputLineB);
+    }
